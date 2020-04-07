80eefb3f0c9fab0fd2da75051ffdfc2177e14c6f
GINER MIGUELEZ Joan <joginermiguelez@ITEM-S51345.home>
2020-04-07 19:50:29 +0200

    Event system added in jitsi instance content type

---
+++ web/modules/custom/videotrucades/src/EventSubscriber/v1/VideotrucadesEvents.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Drupal\videotrucades\EventSubscriber\v1;
+
+use Drupal\hook_event_dispatcher\Event\Entity\EntityUpdateEvent;
+use Drupal\hook_event_dispatcher\Event\Entity\EntityInsertEvent;
+use Drupal\hook_event_dispatcher\Event\Entity\EntityDeleteEvent;
+use Drupal\hook_event_dispatcher\HookEventDispatcherInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\content_moderation\Entity\ContentModerationStateInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\user\Entity\User;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\videotrucades\Services\v1\JitsiManager;
+
+/**
+ * WanNotificationEvents.
+ */
+class VideotrucadesEvents implements EventSubscriberInterface {
+
+  /**
+   * Entity Manager Service.
+   *
+   * @var Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+  /**
+   * A current user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+  /**
+   * NotificationsManager.
+   *
+   * @var \Drupal\wan_notifications\Services\v1\NotificationsManager
+   */
+  protected $JitsiManager;
+
+  /**
+   * Constructor.
+   *
+   * @param Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   Entity Manager.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   A current user instance.
+   * @param \Drupal\videotrucades\Services\v1\JitsiManager $JitsiManager
+   *   Manager class.
+   */
+  public function __construct(
+    EntityTypeManagerInterface $entityTypeManager,
+    AccountProxyInterface $current_user,
+    JitsiManager $JitsiManager
+  ) {
+    $this->entityTypeManager = $entityTypeManager;
+    $this->currentUser = $current_user;
+    $this->JitsiManager = $JitsiManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container)
+  {
+    // Set dependecy injection.
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('current_user'),
+      $container->get('jitsi_manager')
+    );
+  }
+
+  /**
+   * Dispatch notification when node is inserted.
+   *
+   * If node is form specific type, is published and have the boolean activated
+   * then call the FirebaseManager class.
+   *
+   * @param \Drupal\hook_event_dispatcher\Event\Entity\EntityInsertEvent $event
+   *   The event.
+   */
+  public function JitsiInsert(EntityInsertEvent $event)
+  {
+    $entity = $event->getEntity();
+    // When instance is inserted
+    //kint($entity);
+    //die();
+    if ($entity instanceof Node) {
+      $entity_type = $entity->getEntityTypeId();
+      if ($entity_type == 'node') {
+        // Get Node ID, load entity and get bundle.
+        $nid = $entity->id();
+        $node_created = $this->entityTypeManager->getStorage($entity_type)->load($nid);
+        $content_type = $node_created->bundle();
+        if ($content_type == 'jitsi_instance') {
+            $this->JitsiManager->createInstance(['ola']);
+        }
+      }
+    }
+  }
+
+  /**
+   * Entity update.
+   *
+   * @param \Drupal\hook_event_dispatcher\Event\Entity\EntityUpdateEvent $event
+   *   The event.
+   */
+  public function JitsiUpdate(EntityUpdateEvent $event)
+  {
+    $entity = $event->getEntity();
+    // Engagement & High Impact Notification. When entity is updated.
+    if ($entity instanceof Node) {
+      $entity_type = $entity->getEntityTypeId();
+      if ($entity_type == 'node') {
+        // Get Node ID, load entity and get bundle.
+        $nid = $entity->id();
+        $node_updated = $this->entityTypeManager->getStorage($entity_type)->load($nid);
+        $content_type = $node_updated->bundle();
+        if ($content_type == 'jitsi_instance') {
+            $this->JitsiManager->updateInstance(['ola']);
+        }
+      }
+    }
+  }
+
+  /**
+   * Entity update.
+   *
+   * @param \Drupal\hook_event_dispatcher\Event\Entity\EntityDeleteEvent $event
+   *   The event.
+   */
+  public function JitsiDelete(EntityDeleteEvent $event) {
+    $entity = $event->getEntity();
+    if ($entity instanceof Node) {
+      $entity_type = $entity->getEntityTypeId();
+      if ($entity_type == 'node') {
+        // Get Node ID. Load entity. Get Node bundle.
+        $content_type = $entity->bundle();
+        if ($content_type == 'jitsi_instance') {
+          $this->JitsiManager->deleteInstance(['ola']);
+        }
+      }
+    }
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents()
+  {
+    return [
+      HookEventDispatcherInterface::ENTITY_INSERT => 'JitsiInsert',
+      HookEventDispatcherInterface::ENTITY_UPDATE => 'JitsiUpdate',
+      HookEventDispatcherInterface::ENTITY_DELETE => 'JitsiDelete',
+    ];
+  }
+}
